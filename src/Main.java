3f883b288bbf51beb1c12dec7a3ded186a3d8d29
Boldeusz <mnpower@poczta.onet.pl>
2021-01-18 19:16:34 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,49 @@
 public class Main {
+
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value";
+
     public static void main(String[] args) {
 
         System.out.println(getDurationString(65,45));
-        System.out.println(getDurationString(3600));
+        System.out.println(getDurationString(2500L));
+        // System.out.println(getDurationString(3600));
+        System.out.println(getDurationString(-41L));
+        System.out.println(getDurationString(65, 9));
 
     }
 
     private static String getDurationString(long minutes, long seconds){
         if ((minutes < 0) || (seconds < 0 || seconds > 59)){
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
 
         }
         long hours = minutes / 60;
         long remainingMinutes = minutes % 60;
-        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+
+        String hoursString = hours + "h";
+        if (hours < 10){
+            hoursString = "0" + hoursString;
+        }
+
+        String minutesString = remainingMinutes + "m";
+        if (remainingMinutes < 10){
+            minutesString = "0" + minutesString;
+        }
+        String secondsString = seconds + "s";
+        if (seconds < 10){
+            secondsString = "0" + secondsString;
+        }
+
+
+        return hoursString + " " + minutesString + " " + secondsString;
     }
     private static String getDurationString(long seconds){
         if (seconds < 0){
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
         long minutes = seconds / 60;
         long remainingSeconds = seconds % 60;
-        return "In " + seconds + "s there are "+ minutes +"m and " + remainingSeconds + "s";
+        return getDurationString(minutes, remainingSeconds);
+        //return "In " + seconds + "s there are "+ minutes +"m and " + remainingSeconds + "s";
     }
 }
